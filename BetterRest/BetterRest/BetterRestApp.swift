c8dc69eb88b04344adb9043ff7e7ef052c085b12
Jordan Jones <jordan.jones@detroitlabs.com>
2021-06-28 10:58:49 -0400

    Initial BetterRest project
    https://www.hackingwithswift.com/books/ios-swiftui/betterrest-introduction

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Detroit Labs on 6/28/21.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
